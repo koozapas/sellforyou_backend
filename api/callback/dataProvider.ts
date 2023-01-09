cbc5c04b8b76347b097d7d88f0ec0d7f129422c0
DESKTOP-GUALNTB\halin <https://github.com/parksusung>
2023-01-09 16:29:29 +0900

    업데이트

--- api/callback/dataProvider.ts
+++ api/callback/dataProvider.ts
@@ -4,7 +4,7 @@ import { join } from 'path'
 import * as fs from 'fs';
 import { uploadToS3AvoidDuplicateByBuffer, uploadToS3ByBuffer } from "../utils/file_manage";
 import { IPAJobCallbackDoneResponse, IPAJobCallbackResponse, IPAJobCallbackRegistProdResultJson, IPAJobCallbackFailedResultJson, shopDataUrlInfo } from "../playauto_api_type";
-import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { ProductStoreStateEnum } from "../graphql";
 import { publishUserLogData } from "../utils/local/pubsub";
 import { pubsub } from "../utils/helpers";
@@ -15,12 +15,26 @@ export const dataProvider = async (req: Request, res: Response) => {
         
         let productId = req.query.productId;
         let siteCode = req.query.siteCode;
+        const prisma = new PrismaClient();
+        try{
+            
+            return {
+                productId : productId,
+                siteCode :siteCode
+            }
 
-        console.log("productId",productId);
-        console.log("siteCode",siteCode);
+        }
+        catch (e) {
+                console.log('검색 Update Error');
+                // console.log(e);
+                // console.log({ result, config, jobId: response.job_id });
+                res.sendStatus(500)
+                return throwError(errors.etc("connection error"),null);
 
-        res.sendStatus(200);
-        return "img파일";
+        }
+            finally {
+                prisma.$disconnect();
+        }
     }
     catch (e) {
         console.log(e);
