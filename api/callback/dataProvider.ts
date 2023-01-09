58adf54a9230f83a746c59660b3da9bad0a7f4a4
DESKTOP-GUALNTB\halin <https://github.com/parksusung>
2023-01-09 16:47:31 +0900

    connection error

--- api/callback/dataProvider.ts
+++ api/callback/dataProvider.ts
@@ -18,20 +18,20 @@ export const dataProvider = async (req: Request, res: Response) => {
         const prisma = new PrismaClient();
         try{
             let productStore = await prisma.productStore.updateMany({
-                where : { siteCode , productId},
+                where : { siteCode : siteCode , productId : productId},
                 data : { cnt : { increment : 1}}
             })
             if(!productStore) throwError(errors.etc("조회 업데이트 실패"),null);
 
-            return res.json({
-                isSuccess : true,
-                code : 200,
-                queryTest : {
-                    productId : productId,
-                    siteCode :siteCode
-                }
-            });
-            // res.sendFile(join(__dirname,"tiny_white.png"))
+            // return res.json({
+            //     isSuccess : true,
+            //     code : 200,
+            //     queryTest : {
+            //         productId : productId,
+            //         siteCode :siteCode
+            //     }
+            // });
+            res.sendFile(join(__dirname,"tiny_white.png"))
         }
         catch (e) {
                 console.log('검색 Update Error');
