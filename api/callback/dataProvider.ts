b163026218aa8e8fce9f6e502137bd8570c92d99
DESKTOP-GUALNTB\halin <https://github.com/parksusung>
2023-02-01 15:41:07 +0900

    server 롤백

--- api/callback/dataProvider.ts
+++ api/callback/dataProvider.ts
@@ -6,64 +6,63 @@ import { errors, throwError } from "../utils/error";
 
 export const dataProvider = async (req: Request, res: Response) => {
     try {
-        let productId :any = req.query.productId;
-        let siteCode : any= req.query.siteCode;
+        // let productId :any = req.query.productId;
+        // let siteCode : any= req.query.siteCode;
         const prisma = new PrismaClient();
-        // try{
-        //     let ip : any= req.headers['x-real-ip'] || req.connection.remoteAddress;
+        try{
+            let ip : any= req.headers['x-real-ip'] || req.connection.remoteAddress;
          
-        //     let productStoreId = await prisma.productStore.findMany({
-        //         where : { siteCode , productId : parseInt(productId)} 
-        //     }) 
+            // let productStoreId = await prisma.productStore.findMany({
+            //     where : { siteCode , productId : parseInt(productId)} 
+            // }) 
 
-        //     if(!productStoreId) return throwError(errors.etc("Not productStore Id"),null);
+            // if(!productStoreId) return throwError(errors.etc("Not productStore Id"),null);
 
-        //     let productViewLog = await prisma.productViewLog.findMany({
-        //         where : { clientIp : ip , siteCode , productStoreId : productStoreId[0].id} 
-        //     })
+            // let productViewLog = await prisma.productViewLog.findMany({
+            //     where : { clientIp : ip , siteCode , productStoreId : productStoreId[0].id} 
+            // })
 
-        //     if(!productViewLog){
-        //         let productStore = await prisma.productStore.updateMany({
-        //             where : { siteCode : siteCode , productId : parseInt(productId)},
-        //             data : { cnt : { increment : 1} }
-        //         })
+            // if(!productViewLog){
+            //     let productStore = await prisma.productStore.updateMany({
+            //         where : { siteCode : siteCode , productId : parseInt(productId)},
+            //         data : { cnt : { increment : 1} }
+            //     })
 
-        //         if(!productStore) throwError(errors.etc("조회 업데이트 실패"),null);
+            //     if(!productStore) throwError(errors.etc("조회 업데이트 실패"),null);
                 
-        //         await prisma.productViewLog.create({
-        //             data : {
-        //                 clientIp : ip,
-        //                 productStoreId : productStoreId[0].id,
-        //                 siteCode 
-        //             }
-        //         })
+            //     await prisma.productViewLog.create({
+            //         data : {
+            //             clientIp : ip,
+            //             productStoreId : productStoreId[0].id,
+            //             siteCode 
+            //         }
+            //     })
 
-        //     }else{
-        //         const nowdate = new Date();
-        //         let lastViewTime :any = productViewLog[0].viewTime ;
-        //         const diffMSec = nowdate.getTime() - lastViewTime.getTime();
-        //         const diffMin = diffMSec / (60 * 1000);
+            // }else{
+            //     const nowdate = new Date();
+            //     let lastViewTime :any = productViewLog[0].viewTime ;
+            //     const diffMSec = nowdate.getTime() - lastViewTime.getTime();
+            //     const diffMin = diffMSec / (60 * 1000);
 
-        //         if(diffMin > 1440){
+            //     if(diffMin > 1440){
 
-        //             let productStore = await prisma.productStore.updateMany({
-        //                 where : { siteCode : siteCode , productId : parseInt(productId)},
-        //                 data : { cnt : { increment : 1} }
-        //             })
-        //             if(!productStore) throwError(errors.etc("조회 업데이트 실패"),null);
+            //         let productStore = await prisma.productStore.updateMany({
+            //             where : { siteCode : siteCode , productId : parseInt(productId)},
+            //             data : { cnt : { increment : 1} }
+            //         })
+            //         if(!productStore) throwError(errors.etc("조회 업데이트 실패"),null);
 
-        //             await prisma.productViewLog.update({
-        //                 where : {id : productViewLog[0].id },data : { viewTime : new Date()}
-        //             })
+            //         await prisma.productViewLog.update({
+            //             where : {id : productViewLog[0].id },data : { viewTime : new Date()}
+            //         })
                     
-        //         }
-        //     }
+            //     }
+            // }
 
           
             
-            // console.log(ip); //잘받아오는거 확인함 
+            console.log(ip); //잘받아오는거 확인함 
 
-            // console.log(req.url);
             //  2번째  
             // if(typeof window !== "undefined") {
             //     console.log(window.tempProperty);
