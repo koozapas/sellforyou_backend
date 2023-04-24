2e3842168f8bde3837cbe83461afc96202ab7ab9
leejeongbin <pacheonjigyo@gmail.com>
2023-04-24 14:19:30 +0900

    개인분류 대량수집에 등록기능

--- api/translate_types.ts
+++ api/translate_types.ts
@@ -1,27 +1,28 @@
 export interface ITranslate {
-    requestId: number
-    callbackUrl: string
-    data: ITranslateData[]
+  requestId: number;
+  callbackUrl: string;
+  data: ITranslateData[];
 }
 
 export interface ITranslateData {
-    attr : string
-    taobaoNumIid: string
-    title: string
-    optionName: ITranslateOptionName[]
-    optionValue: ITranslateOptionValue[]
-    video: string | null
-    description: string
-    isTranslated: boolean
+  attr: string;
+  taobaoNumIid: string;
+  title: string;
+  optionName: ITranslateOptionName[];
+  optionValue: ITranslateOptionValue[];
+  video: string | null;
+  description: string;
+  isTranslated: boolean;
+  myKeyward: string;
 }
 
 export interface ITranslateOptionName {
-    taobaoPid: string
-    name: string
+  taobaoPid: string;
+  name: string;
 }
 
 export interface ITranslateOptionValue {
-    name: string
-    taobaoVid: string
-    taobaoPid: string
+  name: string;
+  taobaoVid: string;
+  taobaoPid: string;
 }
