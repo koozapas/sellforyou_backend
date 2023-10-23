2a2b419796a5b1d3696137ada054f6c3d53cefb9
leejeongbin <pacheonjigyo@gmail.com>
2023-10-23 14:30:52 +0900

    개인분류 undefined 에러 수정

--- api/utils/local/onebound.ts
+++ api/utils/local/onebound.ts
@@ -515,6 +515,10 @@ export const saveTaobaoItemToUser = async <T extends IFeeInfo>(
             }
           }
           let myKeyward: string | undefined | null = "";
+
+          //이부분 임시로 처리해놓음 - 정빈
+          taobaoData.myKeyward = "";
+
           /** 상품단위 엑셀수집에서 가져온 개인분류 */
           const excelKeyward = taobaoData.myKeyward.replace(/\s*$/, "");
           const translateKeyward =
